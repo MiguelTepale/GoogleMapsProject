03c90f9f5e7c9fa86ec5814bcbd73c1d4dd0f70d
Miguel Tepale <miguel.tepale@gmail.com>
2017-05-31 15:50:25 -0400

    Initial Commit

---
+++ GoogleMapsProject/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  GoogleMapsProject
+//
+//  Created by Miguel Tepale on 5/31/17.
+//  Copyright © 2017 Miguel Tepale. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var toolbarView: UIView!
+    @IBOutlet weak var terrainSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let camera = GMSCameraPosition.camera(withLatitude: <#T##CLLocationDegrees#>, longitude: <#T##CLLocationDegrees#>, zoom: <#T##Float#>)
+        
+        self.mapView.bringSubview(toFront: logoImage)
+        self.mapView.bringSubview(toFront: toolbarView)
+        self.mapView.bringSubview(toFront: terrainSegmentedControl)
+        
+        mapView.mapType = .normal
+    }
+    
+    @IBAction func mapSegmentedAction(_ sender: UISegmentedControl) {
+        switch (sender.selectedSegmentIndex) {
+        case 0:
+            mapView.mapType = .normal
+        case 1:
+            mapView.mapType = .hybrid
+        default:
+            mapView.mapType = .satellite
+        }
+    }
+    
+    
+    
+}
+
