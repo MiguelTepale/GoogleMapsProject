f29ace9ed55041c5164f38fdf0a139b8809e4c6e
Miguel Tepale <miguel.tepale@gmail.com>
2017-06-09 19:50:35 -0400

    All api calls coded in

--- GoogleMapsProject/CustomGMSMarker.swift
+++ GoogleMapsProject/CustomGMSMarker.swift
@@ -13,14 +13,19 @@ class CustomGMSMarker: GMSMarker {
     
     var website: String?
     var imageURLString: String?
+    var addressString: String?
+    var entryIdString: String?
+    var photoIdString: String?
     
-    init(name: String, imageURL: String, latitude: Double, longitude: Double, websiteURL: String) {
+    init(name: String, imageURL: String, latitude: Double, longitude: Double, websiteURL: String, address: String, entryID: String, photoID: String) {
         
         super.init()
         self.title = name
         self.imageURLString = imageURL
         self.position = CLLocationCoordinate2DMake(latitude, longitude)
         self.website = websiteURL
+        self.addressString = address
+        self.entryIdString = entryID
+        self.photoIdString = photoID
     }
-    
 }
--- GoogleMapsProject/ViewController.swift
+++ GoogleMapsProject/ViewController.swift
@@ -13,31 +13,55 @@ class ViewController: UIViewController {
     
     var userResults: [CustomGMSMarker] = []
 
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var logoImage: UIImageView!
     @IBOutlet weak var toolbarView: UIView!
     @IBOutlet weak var terrainSegmentedControl: UISegmentedControl!
     @IBOutlet weak var mapView: GMSMapView!
     
+    var initialFirstSearchInitiated = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
+        searchBar.delegate = self
+        
         loadStockSearchMarkers()
         
         let camera = GMSCameraPosition.camera(withLatitude: 40.708656, longitude: -74.014856, zoom: 16.0)
         mapView.animate(to: camera)
-        let marker = GMSMarker()
-        marker.position = camera.target
-        marker.title = "TurnToTech"
-        marker.snippet = "I hate you GMS"
-        marker.map = mapView
     
         self.mapView.bringSubview(toFront: logoImage)
         self.mapView.bringSubview(toFront: toolbarView)
         self.mapView.bringSubview(toFront: terrainSegmentedControl)
+        self.mapView.bringSubview(toFront: searchBar)
         
         mapView.mapType = .normal
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        searchBar.resignFirstResponder()
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
     }
     
     func loadStockSearchMarkers() {
@@ -57,29 +81,28 @@ class ViewController: UIViewController {
                 if let name = bar["Name"] as? String,
                     let latitude = bar["Latitude"] as? Double,
                     let longitude = bar["Longitude"] as? Double,
-                    let image = bar["Image"] as? String{
+                    let image = bar["Image"] as? String {
                     var website = "https://www.google.com"
+                    var address = "Hello, World!"
+                    var id = "id does not exist"
                     
                     if let websiteExists = bar["Website"] {
                         website = websiteExists as! String
                     }
                     
-                    let barEntry = CustomGMSMarker(name: name, imageURL: image, latitude: latitude, longitude: longitude, websiteURL: website)
+                    if let addressExists = bar["Address"] {
+                        address = addressExists as! String
+                    }
+                    
+                    if let idExists = bar["Id"] {
+                        id = idExists as! String
+                    }
+                    
+                    let barEntry = CustomGMSMarker(name: name, imageURL: image, latitude: latitude, longitude: longitude, websiteURL: website, address: address, entryID: id, photoID: "")
                     barEntry.map = mapView
                     userResults.append(barEntry)
                 }
             }
-        }
-    }
-    
-    @IBAction func mapSegmentedAction(_ sender: UISegmentedControl) {
-        switch (sender.selectedSegmentIndex) {
-        case 0:
-            mapView.mapType = .normal
-        case 1:
-            mapView.mapType = .hybrid
-        default:
-            mapView.mapType = .satellite
         }
     }
     
@@ -98,9 +121,84 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func mapSegmentedAction(_ sender: UISegmentedControl) {
+        switch (sender.selectedSegmentIndex) {
+        case 0:
+            mapView.mapType = .normal
+        case 1:
+            mapView.mapType = .hybrid
+        default:
+            mapView.mapType = .satellite
+        }
+    }
 }
 
-extension ViewController:  GMSMapViewDelegate {
+//FIXME:Figure out why 'searchBarSearchButtonClicked' crashes the app
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        initialFirstSearchInitiated = true
+        searchBar.resignFirstResponder()
+        
+        for search in self.userResults {
+            search.map = nil
+        }
+        self.userResults.removeAll()
+        
+        guard let safeQuery = searchBar.text!.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        
+        let apiString = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.708656,-74.014856&radius=500&types=restaurant&name=\(safeQuery)&key=AIzaSyDhXH99Wt-yp3AaKvZfnlVdL_jekrKAAx8"
+        let apiURL = URL(string: apiString)
+        URLSession.shared.dataTask(with: apiURL!) { data, response, error in
+            
+            guard let jsonData = data, error == nil else {
+                return
+            }
+            
+            guard let decodedJSON = try? JSONSerialization.jsonObject(with: jsonData, options: []) as! [String:Any] else {
+                return
+            }
+            
+            let searchResults = decodedJSON["results"]! as! [[String:Any]]
+            
+            for search in searchResults {
+                
+                guard let name = search["name"] as? String else {
+                    continue
+                }
+                
+                guard let address = search["vicinity"] as? String else {
+                    continue
+                }
+                
+                guard let placeID = search["place_id"] as? String else {
+                    continue
+                }
+                
+                if let geometryDictionary = search["geometry"] as? [String:Any] {
+                    
+                    if let locationDictionary = geometryDictionary["location"] as? [String:Double] {
+                        
+                        let latitude = locationDictionary["lat"]!
+                        let longitude = locationDictionary["lng"]!
+                        
+                        let currentMarker = CustomGMSMarker(name: name, imageURL: "", latitude: 0.0, longitude: 0.0, websiteURL: "", address: address, entryID: placeID, photoID: "")
+                        currentMarker.position.latitude = latitude
+                        currentMarker.position.longitude = longitude
+                        
+                        //Insert gmsMarkers to map and insert values to 'userResults' array
+                        self.userResults.append(currentMarker)
+                        currentMarker.map = self.mapView
+                    }
+                }
+            }
+        }
+        .resume()
+    }
+}
+
+extension ViewController: GMSMapViewDelegate {
     
     func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
         
@@ -108,15 +206,51 @@ extension ViewController:  GMSMapViewDelegate {
         
         let myMarker = marker as! CustomGMSMarker
         customInfoWindow.titleLabel.text = myMarker.title
-        customInfoWindow.detailLabel.text = "Hello World!"
+        customInfoWindow.detailLabel.text = myMarker.addressString
         
-        if let imageURLExists = myMarker.imageURLString {
-            customInfoWindow.searchImageIcon.image = UIImage(named:imageURLExists)
+        if initialFirstSearchInitiated {
+            
+            let placeID = myMarker.entryIdString!
+            let apiString = "https://maps.googleapis.com/maps/api/place/details/json?placeid=\(placeID)&key=AIzaSyDhXH99Wt-yp3AaKvZfnlVdL_jekrKAAx8"
+            let apiURL = URL(string: apiString)
+            
+            URLSession.shared.dataTask(with: apiURL!) { data, response, error in
+                
+                guard let jsonData = data, error == nil else {
+                    return
+                }
+                
+                guard let decodedJSON = try? JSONSerialization.jsonObject(with: jsonData, options: []) as! [String:Any] else {
+                    return
+                }
+            
+                let searchResults = decodedJSON["result"]! as! [String:Any]
+                let photos = searchResults["photos"]! as! [[String:Any]]
+                
+                for photo in photos {
+                    
+                    var photoReferenceString = "apple-icon-180x180.png"
+                    
+                    if let photoReferenceExists = photo["photo_reference"] {
+                        photoReferenceString = photoReferenceExists as! String
+                        myMarker.photoIdString = photoReferenceString
+                        print(myMarker.photoIdString!)
+                        break
+                    }
+                }
+                DispatchQueue.main.async(execute: {
+                    customInfoWindow.searchImageIcon.image = UIImage(named:myMarker.photoIdString!)
+                })
+            }
+            .resume()
+        } else {
+            
+            if let imageURLExists = myMarker.imageURLString {
+                customInfoWindow.searchImageIcon.image = UIImage(named:imageURLExists)
+            } else {
+                customInfoWindow.searchImageIcon.image = UIImage(named:"apple-icon-180x180.png")
+            }
         }
-        else {
-            customInfoWindow.searchImageIcon.image = UIImage(named:"apple-icon-180x180.png")
-        }
-        
         return customInfoWindow
     }
     
@@ -124,15 +258,48 @@ extension ViewController:  GMSMapViewDelegate {
         
         let currentMarker = marker as? CustomGMSMarker
         
+        if initialFirstSearchInitiated {
+            
+            let placeID = currentMarker!.entryIdString!
+            let apiString = "https://maps.googleapis.com/maps/api/place/details/json?placeid=\(placeID)&key=AIzaSyDhXH99Wt-yp3AaKvZfnlVdL_jekrKAAx8"
+            let apiURL = URL(string: apiString)
+            
+            URLSession.shared.dataTask(with: apiURL!) { data, response, error in
+            
+                guard let jsonData = data, error == nil else {
+                    return
+                }
+                
+                guard let decodedJSON = try? JSONSerialization.jsonObject(with: jsonData, options: []) as! [String:Any] else {
+                    return
+                }
+                
+                var website = "https://www.google.com"
+                let searchResults = decodedJSON["result"]! as! [String:Any]
+                if let websiteExists = searchResults["website"] {
+                    website = websiteExists as! String
+                }
+                
+                print(website)
+                currentMarker?.website = website
+                
+                DispatchQueue.main.async(execute: {
+                self.performSegue(withIdentifier: "showWebViewController", sender: currentMarker)
+                })
+            }
+            .resume()
+        } else {
         //Option #1
         performSegue(withIdentifier: "showWebViewController", sender: currentMarker)
-        
-        //Option #2
+        }
+    }
+}
+
+
+//Option #2
 //        let storyboard = UIStoryboard(name: "Main", bundle: nil)
 //        let webViewNavController = storyboard.instantiateViewController(withIdentifier:"WebNav") as! UINavigationController
 //        let webViewController = webViewNavController.viewControllers[0] as! WebViewController
 //        webViewController.userUrl = currentMarker?.website
 //
 //        self.present(webViewNavController, animated: true, completion: nil)
-    }
-}
